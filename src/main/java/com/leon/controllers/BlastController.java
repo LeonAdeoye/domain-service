cf2ffb5f63faad4d90e1f005e3ecdd3c77b2ff5c
LeonAdeoye <leon.adeoye@gmail.com>
2023-10-03 15:27:23 +0800

    cleanup

--- src/main/java/com/leon/controllers/BlastController.java
+++ src/main/java/com/leon/controllers/BlastController.java
@@ -8,9 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.List;
-
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
