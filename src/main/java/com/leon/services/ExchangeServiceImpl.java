0632262d547984f751ee490ce6453631a9224662
LeonAdeoye <leon.adeoye@gmail.com>
2025-05-31 00:12:06 +0800

    exchange service

--- src/main/java/com/leon/services/ExchangeServiceImpl.java
+++ src/main/java/com/leon/services/ExchangeServiceImpl.java
@@ -5,10 +5,13 @@ import com.leon.repositories.ExchangeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ExchangeServiceImpl implements ExchangeService {
     private static final Logger logger = LoggerFactory.getLogger(ExchangeServiceImpl.class);
     @Autowired
